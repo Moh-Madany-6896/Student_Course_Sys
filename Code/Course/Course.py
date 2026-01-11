ae2c17b0addba050ddd8c3091477a0dd3660cc35
Moh-Madany-6896 <aucid2050@gmail.com>
2026-01-11 15:11:41 +0200

    s

--- Code/Course/Course.py
+++ Code/Course/Course.py
@@ -1,17 +1,28 @@
 class Course:
     _Course_ID = 1
+
     def __init__(self, name):
         self.Course_id = Course._Course_ID
         Course._Course_ID += 1
         self.name = name
         self.enrolled_students = []
+    
+    def __str__(self):
+        return f"Your Course ID is: {self.Course_id}, Name: {self.name}, Enrolled Students: {len(self.enrolled_students)}."
+    
+    def __repr__ (self):
+        return f"Your Course ID is: {self.Course_id}, Name: {self.name}, Enrolled Students: {len(self.enrolled_students)}."
+    
     def add_student (self, student):
-        if student.lower ().strip () in self.enrolled_students:
-            print ("The Stydent already !!!")
+        if student.lower().strip() in self.enrolled_students:
+            print (f"The Student \"{student}\" is already added in Course: {self.name} !!!")
         else:
-            self.grade [course_name] = grade
-    def enroll_course (self, course):
-        if course in self.enrolled_courses:
-            print ("The course already added !!!")
+            self.enrolled_students.append (student)
+            print (f"The Student \"{student}\" added Successfully to Course: {self.name} !!!")
+    
+    def remove_Student (self, student):
+        if student.lower().strip() in self.enrolled_students:
+            self.enrolled_students.remove (student)
+            print (f"The Student \"{student}\" removed successfully from Course: {self.name} !!!")
         else:
-            self.enrolled_courses.append (course)
+            print (f"The Student \"{student}\" is NOT in Course: {self.name} !!!")
--- Code/Student/Student.py
+++ Code/Student/Student.py
@@ -6,13 +6,22 @@ class Student:
         self.name = name
         self.grade = {}
         self.enrolled_courses = []
+    
+    def __str__(self):
+        return f"Your Student ID is: {self.Student_id}, Name: {self.name}, Your Grades: {self.grade}, Your Courses: {self.enrolled_courses}."
+    
+    def __repr__(self):
+        return f"Your Student ID is: {self.Student_id}, Name: {self.name}, Your Grades: {self.grade}, Your Courses: {self.enrolled_courses}."
+    
     def add_grade (self, course_name, grade):
         if 0 >= grade >= 100:
             print ("The Grade Must Be Between 0 : 100 !!!")
         else:
             self.grade [course_name] = grade
+    
     def enroll_course (self, course):
         if course in self.enrolled_courses:
             print ("The course already added !!!")
         else:
             self.enrolled_courses.append (course)
+            return self.enrolled_courses
