1053734d4877d4ed96fd78f46615ad0078f35393
Moh-Madany-6896 <aucid2050@gmail.com>
2026-01-13 08:09:12 +0200

    s

--- Code/Core/Sysytem_Manager.py
+++ Code/Core/Sysytem_Manager.py
@@ -34,15 +34,47 @@ class Sys_Manger:
         print ("The course added Successfuly.")
         return course.Course_id
     
-    def remove_course (self, cou_ID):
-        if cou_ID in self.course:
-            course = self.course (cou_ID)
+    def remove_course (self, crs_ID):
+        if crs_ID in self.course:
+            course = self.course (crs_ID)
             if not course.enrolled_students:
-                del self.course [cou_ID]
-                print (f"The Course: {cou_ID} Deleted Successfully.")
+                del self.course [crs_ID]
+                print (f"The Course: {crs_ID} Deleted Successfully.")
             else:
-                print (f"SORRY!! The Course: {cou_ID} has enrolled students and can't be deleted.")
+                print (f"SORRY!! The Course: {crs_ID} has enrolled students and can't be deleted.")
         else:
-            print (f"this ID: {cou_ID} not Registered in the courses")
+            print (f"this ID: {crs_ID} not Registered in the courses")
     
+    def enroll_course (self, std_id, crs_id):
+        if std_id in self.student and crs_id in self.course:
+            student = self.student (std_id)
+            course  = self.course (crs_id)
+            if course.name not in student.enrolled_courses:
+                student.enroll_course (course.name)
+                course.add_student (student.name)
+                print (f"The Course: {crs_id} added to the student: {std_id} Successfully.")
+            else:
+                print (f"The Course: {crs_id} is already added to the student: {std_id}.")
+        else:
+            print ("The Student OR Course not registered!!!")
+
+    def search_course (self, crs_name):
+        for x in self.course.values ()
+            if crs_name.lower().strip() == x.name.lower().strip():
+                return x.name
     
+
+    def add_grade (self, std_id, crs_id, grade):
+        if std_id in self.student and crs_id in self.course:
+            student = self.student (std_id)
+            course = self.course (crs_id)
+            student.add_grade (course.name, grade)
+            print (f"the Course: {crs_id} grade added successfuly.")
+        else:
+            print ("The Student OR Course not registered!!!")
+    
+    def show_all_students (self):
+        return list (self.student.values ())
+    
+    def show_all_courses (self):
+        return list (self.course.values ())
